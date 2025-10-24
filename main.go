b6e0d6df7d3497d5a0f5063dde0f904183803caf
Pritesh Tupe <mail@priteshtupe.com>
2025-10-24 16:15:50 -0400

    Add fuel log form input logic

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := app.New().NewWindow("Duri")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	odoInput := widget.NewEntry()
+	fuelInput := widget.NewEntry()
+	dateInput := &widget.DateEntry{}
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "Odometer (km)", Widget: odoInput},
+			{Text: "Fuel (ltr)", Widget: fuelInput},
+			{Text: "Date", Widget: dateInput},
+		},
+		OnSubmit: func() {
+			log.Println("Form submitted:", odoInput.Text, fuelInput.Text, dateInput.Text)
+		},
+	}
+
+	w.SetContent(form)
 	w.ShowAndRun()
 }
