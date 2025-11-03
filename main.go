f70c60b7fc562c279ade650606d0860e8f253856
Pritesh Tupe <mail@priteshtupe.com>
2025-11-02 20:24:21 -0500

    Add fuel log input page

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
+	"strconv"
+	"time"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
+type FuelLog struct {
+	odometer int
+	fuel     float64
+	date     time.Time
+}
+
 func main() {
-	w := app.New().NewWindow("Duri")
+	a := app.New()
+	w := a.NewWindow("Duri")
 
 	odoInput := widget.NewEntry()
 	fuelInput := widget.NewEntry()
@@ -22,9 +32,24 @@ func main() {
 		},
 		OnSubmit: func() {
 			log.Println("Form submitted:", odoInput.Text, fuelInput.Text, dateInput.Text)
+			odometer, err := strconv.Atoi(odoInput.Text)
+			if err != nil {
+				panic(err)
+			}
+			fuel, err := strconv.ParseFloat(fuelInput.Text, 64)
+			if err != nil {
+				panic(err)
+			}
+			date, err := time.Parse("02/01/2006", dateInput.Text)
+			if err != nil {
+				panic(err)
+			}
+			fl := FuelLog{odometer, fuel, date}
+			log.Println(fl)
 		},
 	}
 
+	w.Resize(fyne.NewSize(512, 512))
 	w.SetContent(form)
 	w.ShowAndRun()
 }
